148d7c973ef95903aeddba1efc0316f3cc9a491b
Leon <leonndp@gmail.com>
2020-03-02 14:10:40 +0800

    Initial commit

---
+++ src/containers/App/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './App.module.css';
+
+function App() {
+    return (
+        <div>
+            App setup
+
+        </div>
+    );
+}
+
+export default App;
